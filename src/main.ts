0d3651041449004230cf00bca3f8a8e71344ba5b
Roopa Mohan <rmohan@salesforce.com>
2023-02-28 15:08:39 -0800

    trying to comment on PR

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
     const jsonStr: string = core.getInput('jsonstring');
 
     core.info(`json string received: ${jsonStr}`);
+
+    await commentTest();
     
     // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
@@ -16,6 +19,21 @@ async function run(): Promise<void> {
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
+}
+
+async function commentTest(): Promise<void> {
+  const pullRequest = github.context.payload;
+
+  const githubToken = core.getInput('github_token');
+  const octoKit = github.getOctokit(githubToken);
+
+  await octoKit.rest.pulls.createReviewComment({
+    ...github.context.repo,
+    pull_number: pullRequest.number,
+    body: "Test data",
+    path: "force-app/main/default/classes/Cat.cls",
+    line: 3
+  });
 }
 
 run()
