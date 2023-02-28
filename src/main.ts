24d33cf21c0a421a0a4e0b1f10a22e5f366ddaf3
Roopa Mohan <rmohan@salesforce.com>
2023-02-28 13:56:48 -0800

    Creating a starting point

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const jsonStr: string = core.getInput('jsonstring');
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.info(`json string received: ${jsonStr}`);
+    
+    // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.setOutput('time', new Date().toTimeString())
+    // core.debug(new Date().toTimeString())
+    // await wait(parseInt(ms, 10))
+    // core.debug(new Date().toTimeString())
+
+    // core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
